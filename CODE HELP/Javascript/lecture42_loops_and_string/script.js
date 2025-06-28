4fd68d8fcb66e1434ee8903fe7cf6aa4f573525f
devAmulya <amulyagupta57121@gmail.com>
2025-06-28 09:17:49 +0530

    Loops and strings

---
+++ CODE HELP/Javascript/lecture42_loops_and_string/script.js
@@ -0,0 +1,47 @@
+// console.log("Loops and Strings");
+// for(let a = 0; a < 10; a++){
+//     console.log(a);
+// }
+// console.log("Reverse Counting");
+// let a = 10;
+// while(a>0){
+//     if(a%2 == 0){
+//         console.log('Even')
+//     }
+//     else{
+//         console.log(a);
+//     }
+//     a--;
+//     if(a<5){
+//         break;
+//     }
+// }
+
+// let a =1;
+// do{
+//     console.log("DO while loop");
+//     a++;
+// }while(a<=10);
+
+// STRINGS
+
+let firstName = "Amulya";
+let lastName = 'Gupta';
+let name_msg = `My name is 
+Amulya Gupta.
+Hello`
+console.log(name_msg);
+
+let fN = new String("Amulya");
+console.log(fN);
+
+let op1 = 'English ';
+let op2 = 'Hindi';
+let ans = op1 + op2;
+console.log(ans);
+console.log(ans.length);
+console.log(op1.toUpperCase());
+console.log(op2.toLowerCase());
+let finalAns = `${op1} and ${op2}`;
+console.log(finalAns);
+
