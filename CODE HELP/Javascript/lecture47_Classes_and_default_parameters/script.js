4f5d1622f8f53e0d8518fc0fd704e93ad6109b74
devAmulya <amulyagupta57121@gmail.com>
2025-07-04 17:02:45 +0530

    Classes and default parameters

--- CODE HELP/Javascript/lecture47_Classes_and_default_parameters/script.js
+++ CODE HELP/Javascript/lecture47_Classes_and_default_parameters/script.js
@@ -4,6 +4,11 @@ class Human{
     #wt = 80; //private
     ht = 180;
 
+    constructor(newAge, newHeight){
+        this.age = newAge;
+        this.ht = newHeight;
+    }
+
     // behaviour
     walking(){
         console.log("I am walking", this.#wt);
@@ -11,9 +16,19 @@ class Human{
     running(){
         console.log("I am running");
     }
+
+    get fetchWeight(){
+        return this.#wt;
+    }
+    set modifyWeight(val){
+        this.#wt = val;
+    }
 }
 
 let obj = new Human();
 console.log(obj.age);
 obj.walking();
+obj.fetchWeight()
 console.log(obj.wt);
+
+// Function parameters 
