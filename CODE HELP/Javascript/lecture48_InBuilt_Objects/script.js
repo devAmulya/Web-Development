b2531b4b2e9b607322b8cf7ad0e7498decf1ba26
devAmulya <amulyagupta57121@gmail.com>
2025-07-05 21:38:10 +0530

    InBuilt Objects

---
+++ CODE HELP/Javascript/lecture48_InBuilt_Objects/script.js
@@ -0,0 +1,21 @@
+console.log("babbar");
+console.log(Math.PI);
+console.log(Math.max(60,30,24,600,712,89));
+console.log(Math.min(60,30,24,600,712,89));
+console.log(Math.round(1.4));
+console.log(Math.ceil(1.1));
+console.log(Math.floor(3.2));
+console.log(Math.abs(-5));
+console.log(Math.random());
+ console.log(Math.sqrt(8));
+ console.log(Math.pow(2,10));
+ console.log(Math.sin(Math.PI/2));
+ console.log(Math.cos(0));
+ console.log(Math.tan(45));
+
+ let curr = new Date();
+ let date = new Date('June 20 1998 07:15');
+ let newDate = new Date(1998, 5, 20, 7);
+ console.log(newDate);
+ console.log(newDate.getFullYear());
+ 
