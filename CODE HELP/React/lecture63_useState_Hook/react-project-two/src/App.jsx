3c160ae9679bec27be1005ea32cd82d3ca15729e
devAmulya <amulyagupta57121@gmail.com>
2025-08-06 20:43:57 +0530

    propd children

--- CODE HELP/React/lecture63_useState_Hook/react-project-two/src/App.jsx
+++ CODE HELP/React/lecture63_useState_Hook/react-project-two/src/App.jsx
@@ -1,9 +1,25 @@
+import {useState} from 'react';
 import './App.css'
 import Counter from './components/Counter'
+import Card from './components/Card'
+import Button from './components/Button'
+
 function App() {
+  const [count,setCount] = useState(0);
+  function handleClick(){
+    setCount(count+1);
+  }
   return (
     <div>
+      <Button handleClick = {handleClick}>
+        <h1>{count}</h1>
+      </Button>
       <Counter />
+      <Card name = "Amulya Gupta">
+        <h1>Best Web Dev Learning</h1>
+        <p>Trying to be consistent in this</p>
+        <p>Will complete the course soon</p>
+      </Card>
     </div>
   )
 }
